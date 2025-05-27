a78fbf8d68b53d273a5d51c08674ff564cbf9e4a
jovanaceganjac <jceganjac7@gmail.com>
2025-05-27 12:57:44 +0200

    Create GoreDesnoCev.java

---
+++ popravniv1/GoreDesnoCev.java
@@ -0,0 +1,34 @@
+package igra;
+
+import java.awt.Graphics;
+
+public class GoreDesnoCev extends Cev {
+
+	@Override
+	public void crtajUnutrasnjost(Graphics g) {
+		g.fillRect(this.getWidth()/4, this.getHeight()/4, this.getWidth()/2, this.getHeight());
+		g.fillRect(this.getWidth()/4, this.getHeight()/4, this.getWidth(), this.getHeight()/2);
+		
+	}
+
+	@Override
+	public void crtajIvice(Graphics g) {
+		g.drawLine(3*this.getWidth()/4, 3*this.getHeight()/4, this.getWidth(), 3*this.getHeight()/4);
+		g.drawLine(3*this.getWidth()/4, 3*this.getHeight()/4, 3*this.getWidth()/4, this.getHeight());
+		g.drawLine(this.getWidth()/4, this.getHeight()/4, this.getWidth(), this.getHeight()/4);
+		g.drawLine(this.getWidth()/4, this.getHeight()/4, this.getWidth()/4, this.getHeight());
+		
+	}
+
+	@Override
+	public void crtajUsmerenje(Graphics g) {
+		g.drawLine(this.getWidth()/2, this.getHeight()/2, 3*this.getWidth()/4, this.getHeight()/2);
+		g.drawLine(this.getWidth()/2, this.getHeight()/2, this.getWidth()/2, 3*this.getHeight()/4);
+		int xKrajLinije=3*this.getWidth()/4;
+		int yKrajLinije=this.getHeight()/2;
+		
+		int x[]= {xKrajLinije, xKrajLinije+10, xKrajLinije};
+		int y[]= {yKrajLinije+4, yKrajLinije, yKrajLinije-4};
+		g.fillPolygon(x, y, 3);
+	}
+}
